137b92dfb3fddc014d79bea5c3ca88c53d63e6d2
Mohammed Amine AYACHE <med.amine.ayache@gmail.com>
2022-03-30 23:43:42 +0200

    Version 1

--- src/main/java/com/project/pizzeria/UserAdminController.java
+++ src/main/java/com/project/pizzeria/UserAdminController.java
@@ -2,14 +2,10 @@ package com.project.pizzeria;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Singleton;
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -20,49 +16,24 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.project.pizzeria.beans.Token;
 import com.project.pizzeria.beans.User;
 import com.project.pizzeria.exeptions.ExceptionHandler;
 import com.project.pizzeria.services.TokenService;
 import com.project.pizzeria.services.UserService;
-import com.project.pizzeria.utils.Constants;
-import com.project.pizzeria.utils.enumuration.UserRole;
 
-@Path("/")
+@Path("/api")
 @Singleton
-public class UserController {
+public class UserAdminController {
 	@Context
     HttpServletRequest request;
 	
 	private static UserService userService = new UserService();
-	private static TokenService tokenService = new TokenService();
-
 	
-	@Path("/")
-	@DELETE
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response signout()
-	{
-		String tokenString = request.getHeader(Constants.AUTHORIZATION_HEADER);
-		tokenString=tokenString.substring(Constants.AUTHORIZATION_PREFIX.length());
-		try {
-			Token token = tokenService.checkTokenValidity(tokenString);
-			if(!userService.logout(token))
-				throw new SQLException("Unsuccessfull");
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return Response.status(Response.Status.BAD_REQUEST).entity(
-					new ExceptionHandler(e)).build();
-		}
-		return Response.status(Response.Status.OK).build();
-	}
-
-	@Path("/auth/register")
+	@Path("/admin/account")
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response register(User user)
+	public Response createAccount(User user)
 	{		
-		user.setRole(UserRole.USER.toString());
 		try {
 			userService.createUser(user);
 		} catch (SQLException e) {
@@ -72,25 +43,52 @@ public class UserController {
 		}
 		return Response.status(Response.Status.CREATED).entity(user).build();
 	}
-
-	@Path("/auth/login")
-	@POST
+	
+	@Path("/admin/account")
+	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response login(User user)
-	{
-		Map<String,Object> map = new HashMap<String,Object>();
-		Token token = new Token();
-		User userDto = new User();
+	public Response getAccounts()
+	{		
+		List<User> list = new ArrayList<User>();
 		try {
-			token= userService.login(user);
-			userDto=userService.findUserByEmail(user.getEmail());
-			map.put("token", token);
-			map.put("user", userDto);
+			 list = userService.findAllUsers();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return Response.status(Response.Status.BAD_REQUEST).entity(
 					new ExceptionHandler(e)).build();
 		}
-		return Response.status(Response.Status.OK).entity(map).build();
+		return Response.status(Response.Status.OK).entity(list).build();
 	}
+	
+	@Path("/admin/account/{id}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAccount(@PathParam("id") Long id )
+	{		
+		User list = null;
+		try {
+			 list = userService.findUserById(id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return Response.status(Response.Status.BAD_REQUEST).entity(
+					new ExceptionHandler(e)).build();
+		}
+		return Response.status(Response.Status.OK).entity(list).build();
+	}
+	
+	@Path("/admin/account/{id}")
+	@DELETE
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response disable(@PathParam("id") Long id)
+	{		
+		try {
+			 userService.disable(id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return Response.status(Response.Status.BAD_REQUEST).entity(
+					new ExceptionHandler(e)).build();
+		}
+		return Response.status(Response.Status.OK).build();
+	}
+	
 }
